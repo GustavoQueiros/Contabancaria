1e23e9abe7d6cbae1d2aa5dd0b3d8df40105be1f
GustavoQueiros <gustavo.dev47@gmail.com>
2023-12-13 17:12:20 -0300

    Projeto conta bancaria

---
+++ src/conta/Menu.java
@@ -0,0 +1,29 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner (System.in);
+		
+		System.out.println(" ****** Menu principal *******");
+		
+		String usuario;
+		System.out.println("Digite seu nome:");
+		usuario = leia.nextLine();
+		
+		String agencia;
+		System.out.println("Digite sua agência:");
+		agencia = leia.nextLine();
+		
+		String conta;
+		System.out.println("Digite sua conta:");
+		conta = leia.nextLine();
+		
+		
+		
+	}
+
+}
