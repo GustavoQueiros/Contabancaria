5d0b8d9d50224ad14a9b2c50833f69ea2ca8056e
GustavoQueiros <gustavo.dev47@gmail.com>
2023-12-20 20:02:06 -0300

    Final do projeto

--- src/conta/repository/ContaRepository.java
+++ src/conta/repository/ContaRepository.java
@@ -9,11 +9,12 @@ public interface ContaRepository {
 	public void procurarPorNome(int numero);
 	public void listartodas();
 	public void cadastrar(Conta conta);
-	public void atualizaar(Conta conta);
+	public void atualizar(Conta conta);
 	public void deletar (int numero);
 	
 	//Métodos Bancários
 	public void sacar (int numero, float valor);
 	public void depositar(int numero, float valor);
 	public void transferir(int numeroOrigem, int numeroDestino, float valor);
+	
 }
